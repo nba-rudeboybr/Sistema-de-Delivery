2cbdab66731e4d3a7727118db331d5a7a5ea2ce5
Nikolas Araújo <nikolas_araujo@live.com>
2025-11-30 15:47:18 -0300

    Remove pasta duplicada e .gitignore

--- Sistema-de-Delivery-main/dish-app-Docker/backend/src/main/java/com/restaurant/order/repository/DishRepository.java
+++
@@ -1,9 +0,0 @@
-package com.restaurant.order.repository;
-
-import com.restaurant.order.model.Dish;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DishRepository extends JpaRepository<Dish, Long> {
-}
